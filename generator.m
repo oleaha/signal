3a1871bff1952450f009ba4a1c808d8b7804d2ee
Ole Andreas Hansen <oleaha@me.com>
2015-01-21 12:52:44 +0100

    Commented code and generated report

--- generator.m
+++ generator.m
@@ -1,18 +1,23 @@
-function generated_array = generator( )
+%% Main function for encoding phonenumber into sound
+function sound_sequence = generator( )
 
-generated_array = []; % The array we want to return containing the different signals
-
-freq = [697, 770, 852, 941, 1209, 1336, 1447];
+% The array we want to return containing the different signals
+sound_sequence = [];
+ % Given sample rate
 Fs = 8000;
-z = 0:1/Fs:.05; % Zero Vektor
-zeroFrequency = 0*z; % Small pause betweend dials
+% Zero Vector
+z = 0:1/Fs:.05;
+% Pause between sound_sequences
+zeroFrequency = 0*z; 
 
-% Table funny
+% Table
 % Fs/Hz | 1209 | 1336 | 1477
 %  697  |  1   |   2  |  3
 %  770  |  4   |   5  |  6
 %  852  |  7   |   8  |  9
 %  941  |  *   |   0  |  #
+% The seven frequences that represent the numbers dialed
+freq = [697, 770, 852, 941, 1209, 1336, 1447];
 
 input_number = input('Enter phonenumber: ', 's');
 
@@ -20,43 +25,44 @@ if input_number;
     for x = 1 : numel(input_number)
         switch input_number(x)
             case '1'
-                generated_array = [generated_array (frequency(freq(1), freq(5)))];               
+                sound_sequence = [sound_sequence (frequency(freq(1), freq(5)))];               
             case '2'
-                generated_array = [generated_array (frequency(freq(1), freq(6)))];
+                sound_sequence = [sound_sequence (frequency(freq(1), freq(6)))];
             case '3'
-                generated_array = [generated_array (frequency(freq(1), freq(7)))];
+                sound_sequence = [sound_sequence (frequency(freq(1), freq(7)))];
             case '4'
-                generated_array = [generated_array (frequency(freq(2), freq(5)))];
+                sound_sequence = [sound_sequence (frequency(freq(2), freq(5)))];
             case '5'
-                generated_array = [generated_array (frequency(freq(2), freq(6)))];
+                sound_sequence = [sound_sequence (frequency(freq(2), freq(6)))];
             case '6'
-                generated_array = [generated_array (frequency(freq(2), freq(7)))];
+                sound_sequence = [sound_sequence (frequency(freq(2), freq(7)))];
             case '7'
-                generated_array = [generated_array (frequency(freq(3), freq(5)))];
+                sound_sequence = [sound_sequence (frequency(freq(3), freq(5)))];
             case '8'
-                generated_array = [generated_array (frequency(freq(3), freq(6)))];
+                sound_sequence = [sound_sequence (frequency(freq(3), freq(6)))];
             case '9'
-                generated_array = [generated_array (frequency(freq(3), freq(7)))];
+                sound_sequence = [sound_sequence (frequency(freq(3), freq(7)))];
             case '*'
-                generated_array = [generated_array (frequency(freq(4), freq(5)))];
+                sound_sequence = [sound_sequence (frequency(freq(4), freq(5)))];
             case '0'
-                generated_array = [generated_array (frequency(freq(4), freq(6)))];
+                sound_sequence = [sound_sequence (frequency(freq(4), freq(6)))];
             case '#'
-                generated_array = [generated_array (frequency(freq(4), freq(7)))];
+                sound_sequence = [sound_sequence (frequency(freq(4), freq(7)))];
             otherwise
                 disp('Illegal input')
         end % End switch
-        generated_array = [generated_array (zeroFrequency)];
+        sound_sequence = [sound_sequence (zeroFrequency)];
     end % End for
-    sound(generated_array)
+    sound(sound_sequence)
 end % End if
 end % End function
 
-% Simple function to add simplicity
+%% Function for combining two frequencies
 function frequency = frequency(freqOne, freqTwo)
-    T = 0.2; % Tid for hver lyd
-    Fs = 8000; % Samplingsrate
+
+    T = 0.2; % Length of signal/frequency/sound in seconds
+    Fs = 8000; % Given sample rate
     t = 0:1/Fs:T; % Sample vektor from 0 to T with steps of 1/Fs
-    frequency = cos(2*pi*freqOne*t) + cos(2*pi*freqTwo*t);
+    frequency = cos(2*pi*freqOne*t) + cos(2*pi*freqTwo*t);  % Combine the two frequencies
 end
 
